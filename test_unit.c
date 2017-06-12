907ab9c975573cef3c68b8ea3a81a8910e913e09
Pavol Rusnak <stick@gk2.sk>
2017-06-12 14:43:58 +0200

    update travis notification url

--- test_unit.c
+++ test_unit.c
@@ -1,6 +1,28 @@
 #include <assert.h>
+#include <stdarg.h>
+#include <stdint.h>
 
 #include "norcow.h"
+
+void assert_content(const void *data, ...)
+{
+    va_list va;
+    va_start(va, data);
+    uint32_t len, pos;
+    uint8_t byte;
+    for (;;) {
+       len = va_arg(va, uint32_t);
+       byte = va_arg(va, uint32_t);
+       if (!len) {
+           break;
+       }
+       // compare len/byte area
+       (void)len;
+       (void)byte;
+       (void)pos;
+    }
+    va_end(va);
+}
 
 bool res;
 
